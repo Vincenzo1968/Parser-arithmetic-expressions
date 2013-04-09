e334915425b7b9f7188af05842a51c04ccff64c8
Vincenzo1968 <vincenzo.locicero@libero.it>
2013-04-09 17:28:57 +0200

    Added COPYING

--- C/ParseExpr.c
+++ C/ParseExpr.c
@@ -1,25 +1,22 @@
-// Copyright (C) 2013
-// Author: Vincenzo Lo Cicero.
-// e-mail: vincenzo.locicero@live.it
-//
-// This program is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-// Public License for more details.
-// You should have received a copy of the GNU General Public License along with
-// this program. If not, see http://www.gnu.org/licenses/.
+/*
+   Copyright (C) 2013 Vincenzo Lo Cicero
 
-// Questo programma è software free: puoi redistribuirlo e/o modificarlo a piacere
-// sotto le condizioni della GNU General Public License così come pubblicata da Free Software
-// Foundation, o la versione 3 della licenza o (a propria scelta) una versione successiva. 
-// Questo programma è distribuito nella speranza che sia utile, ma SENZA ALCUNA GARANZIA;
-// senza nemmeno la garanzia implicita di commerciabilità o idoneità per uno SCOPO PARTICOLARE.
-// Vedi la GNU General Public License per maggiori dettagli. 
-// Dovresti aver ricevuto una copia della GNU General Public License insieme a questo programma.
-// Se questo non è avvenuto, vedi qui: http://www.gnu.org/licenses/.
+   Author: Vincenzo Lo Cicero.
+   e-mail: vincenzo.locicero@live.it
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
--- C/lexer.h
+++ C/lexer.h
@@ -1,3 +1,25 @@
+/*
+   Copyright (C) 2013 Vincenzo Lo Cicero
+
+   Author: Vincenzo Lo Cicero.
+   e-mail: vincenzo.locicero@live.it
+
+   This file is part of ParseExprLib.
+
+   ParseExprLib is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ParseExprLib is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with ParseExprLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef LEXER_H
 #define LEXER_H
 
